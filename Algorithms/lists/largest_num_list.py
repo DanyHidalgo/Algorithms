14f5c2f73ea08de240071a703a26b08858d5e518
DanyHidalgo <danielhl@ufm.edu>
2022-05-06 18:19:12 -0600

    largest_num_list.py added

---
+++ Algorithms/lists/largest_num_list.py
@@ -0,0 +1,14 @@
+'''
+Print the largest number of a list
+'''
+
+def largest_num_list(ele):
+
+    max = ele[0]
+
+    for i in range (len(ele)):
+        ele[i]
+        if(ele[i]>max):
+            max = ele[i]
+
+    print("The largest number in the list is:", max)
