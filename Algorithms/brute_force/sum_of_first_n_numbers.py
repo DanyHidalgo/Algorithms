1c04a5c001565fe522d4369061da6ccc6b1041d0
DanyHidalgo <danielhl@ufm.edu>
2022-05-06 18:06:42 -0600

    sum_of_first_n_numbers.py added

---
+++ Algorithms/brute_force/sum_of_first_n_numbers.py
@@ -0,0 +1,19 @@
+'''
+Print and calculate the sum of the first n numbers
+'''
+
+
+def sum_of_first_n_numbers():
+
+
+    n = int(input("Input a n number: "))
+    print("n:", n)
+
+    sum = 0
+
+    for num in range(n + 1):
+
+        sum += num
+
+    print("Sum:", sum)
+
